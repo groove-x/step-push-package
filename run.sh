7a64142b697a5e7422a239c50b72c9b354e8c549
Ato Araki <ato.araki@groove-x.com>
2017-06-07 15:54:39 +0900

    add token option

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+export PACKAGECLOUD_TOKEN=${WERCKER_PUSH_PACKAGE_TOKEN}
 for pkg in $(find ${WERCKER_PUSH_PACKAGE_PATH} -name "*.deb"); do
   package_cloud yank ${WERCKER_PUSH_PACKAGE_REPO_NAME} $(basename ${pkg}) || true
   package_cloud push ${WERCKER_PUSH_PACKAGE_REPO_NAME} ${pkg}  
