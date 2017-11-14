ba2035bfed3e426065baf93c8827056fb1f6c840
Junya Hayashi <junya.hayashi@groove-x.com>
2017-11-14 15:43:23 +0900

    delete old packages

--- run.sh
+++ run.sh
@@ -3,7 +3,46 @@
 set -e
 
 export PACKAGECLOUD_TOKEN=${WERCKER_PUSH_PACKAGE_TOKEN}
-for pkg in $(find ${WERCKER_PUSH_PACKAGE_PATH} -name "*.deb"); do
-  package_cloud yank ${WERCKER_PUSH_PACKAGE_REPO_NAME} $(basename ${pkg}) || true
-  package_cloud push ${WERCKER_PUSH_PACKAGE_REPO_NAME} ${pkg}  
-done
+
+install_jq () {
+  command -v jq || apt-get install --yes jq
+}
+
+extract_repo_name () {
+  parts=(${WERCKER_PUSH_PACKAGE_REPO_NAME//\// })
+  USER_REPO=${parts[0]}/${parts[1]}
+  DISTRO_VERSION=${parts[2]}/${parts[3]}
+}
+
+delete_old () {
+  version=`dpkg-deb -f ${1} Version`
+  pkg_name=`dpkg-deb -f ${1} Package`
+  arch=`dpkg-deb -f ${1} Architecture`
+  base_version=`echo ${version} | cut -d '-' -f 1`
+
+  endpoint="https://packagecloud.io/api/v1/repos/${USER_REPO}/package/deb/${DISTRO_VERSION}/${pkg_name}/${arch}/versions.json"
+  res=`curl -u ${PACKAGECLOUD_TOKEN}: ${endpoint}`
+
+  old_vers=(`echo ${res} | jq -r ".[].version"`)
+  destroy_urls=(`echo ${res} | jq -r ".[].destroy_url"`)
+
+  for idx in ${!old_vers[@]}
+  do
+   if [[ ${old_vers[idx]} == ${base_version} ]]
+   then
+     echo destroy: https://packagecloud.io${destroy_urls[idx]}
+     curl -u ${PACKAGECLOUD_TOKEN}: -X DELETE https://packagecloud.io${destroy_urls[idx]}
+   fi
+  done
+}
+
+main () {
+  install_jq
+  extract_repo_name
+  for pkg in $(find ${WERCKER_PUSH_PACKAGE_PATH} -name "*.deb"); do
+    delete_old ${pkg}
+    package_cloud push ${WERCKER_PUSH_PACKAGE_REPO_NAME} ${pkg}
+  done
+}
+
+main
