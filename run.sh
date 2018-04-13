a6e40cf0cfcbc0a8b0bdc0c2f6a74c479675ec3d
Junya Hayashi <junya.hayashi@groove-x.com>
2018-04-13 11:22:08 +0900

    Revert "Merge pull request #8 from groove-x/feature/add-delete-option"
    This reverts commit 4bbf6541b483f470f3f5f317fc5d62b9da869403, reversing
    changes made to 5f234947e04d988c3c16665dfba4fca330340b74.

--- run.sh
+++ run.sh
@@ -15,31 +15,29 @@ extract_repo_name () {
 }
 
 delete_old () {
-  pkg_filename=`basename -- ${1}`
-  pkg_version=`dpkg-deb -f ${1} Version`
+  version=`dpkg-deb -f ${1} Version`
   pkg_name=`dpkg-deb -f ${1} Package`
-  pkg_arch=`dpkg-deb -f ${1} Architecture`
-  pkg_base_version=`echo ${pkg_version} | cut -d '-' -f 1`
+  arch=`dpkg-deb -f ${1} Architecture`
+  base_version=`echo ${version} | cut -d '-' -f 1`
 
-  endpoint="https://packagecloud.io/api/v1/repos/${USER_REPO}/package/deb/${DISTRO_VERSION}/${pkg_name}/${pkg_arch}/versions.json"
-  res=`curl -u ${PACKAGECLOUD_TOKEN}: ${endpoint} -s`
+  endpoint="https://packagecloud.io/api/v1/repos/${USER_REPO}/package/deb/${DISTRO_VERSION}/${pkg_name}/${arch}/versions.json"
+  res=`curl -u ${PACKAGECLOUD_TOKEN}: ${endpoint}`
 
-  res_length=(`echo ${res} | jq "length"`)
-  if [ $res_length -eq 0 ]; then
+  old_vers_len=(`echo ${res} | jq "length"`)
+  if [ $old_vers_len -eq 0 ]; then
       return
   fi
 
-  versions=(`echo ${res} | jq -r ".[].version"`)
-  filenames=(`echo ${res} | jq -r ".[].filename"`)
+  old_vers=(`echo ${res} | jq -r ".[].version"`)
   destroy_urls=(`echo ${res} | jq -r ".[].destroy_url"`)
 
-  for idx in ${!versions[@]}
+  for idx in ${!old_vers[@]}
   do
-    if [[ ${versions[idx]} == ${pkg_base_version} && ${filenames[idx]} != ${pkg_filename} ]]
-    then
-      echo destroy: https://packagecloud.io${destroy_urls[idx]}
-      curl -u ${PACKAGECLOUD_TOKEN}: -X DELETE https://packagecloud.io${destroy_urls[idx]}
-    fi
+   if [[ ${old_vers[idx]} == ${base_version} ]]
+   then
+     echo destroy: https://packagecloud.io${destroy_urls[idx]}
+     curl -u ${PACKAGECLOUD_TOKEN}: -X DELETE https://packagecloud.io${destroy_urls[idx]}
+   fi
   done
 }
 
@@ -47,10 +45,8 @@ main () {
   install_jq
   extract_repo_name
   for pkg in $(find ${WERCKER_PUSH_PACKAGE_PATH} -name "*${WERCKER_PUSH_PACKAGE_ARCH}.deb"); do
+    delete_old ${pkg}
     package_cloud push ${WERCKER_PUSH_PACKAGE_REPO_NAME} ${pkg}
-    if [[ "${WERCKER_PUSH_PACKAGE_DELETE_OLD}" != "false" ]]; then
-      delete_old ${pkg}
-    fi
   done
 }
 
